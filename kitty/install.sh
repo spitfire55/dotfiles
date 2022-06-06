ceac5335ed687279c065121067f0a4d08e95e0c2
Dale Lakes <spitfire94155@gmail.com>
2022-06-06 12:18:41 -0400

    Fix kitty install script

--- kitty/install.sh
+++ kitty/install.sh
@@ -4,6 +4,6 @@ kitty_config_file=~/.config/kitty
 
 mkdir -p ~/.config/
 
-if [ ! -L "$i3_config_file" ]; then
+if [ ! -L "$kitty_config_file" ]; then
 	ln -s ~/.dotfiles/kitty/kitty "$kitty_config_file"
 fi
