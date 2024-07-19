59c8cce8547af4167100c0aac759692ee0a7dc98
Dale Lakes <dale.f.lakes@gmail.com>
2024-07-19 10:20:35 -0700

    Fix image.nvim

--- nvim/nvim/lua/deviathan/lazy/image.lua
+++ nvim/nvim/lua/deviathan/lazy/image.lua
@@ -1,19 +1,7 @@
-return {
-  {
-    "vhyrro/luarocks.nvim",
-    lazy = false,
-    priorit = 1000,
-    opts = {
-      rocks = { "magick" },
-    },
-  },
-  {
-    "3rd/image.nvim",
-    dependencies = { "luarocks.nvim" },
-    config = function()
-      require("image").setup({
-        backend = "kitty",
-      })
-    end,
-  },
+return
+{
+  "3rd/image.nvim",
+  config = function()
+    require("image").setup()
+  end,
 }
