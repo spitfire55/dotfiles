bb9d4999eee3be99130a8136871552a827c265cd
Dale Lakes <dale.f.lakes@gmail.com>
2024-08-13 17:57:08 -0700

    Switch to tokyonight and fix image.lua

--- nvim/nvim/lua/deviathan/lazy/image.lua
+++ nvim/nvim/lua/deviathan/lazy/image.lua
@@ -1,7 +1,15 @@
-return
-{
-  "3rd/image.nvim",
-  config = function()
-    require("image").setup()
-  end,
+return {
+  {
+    "vhyrro/luarocks.nvim",
+    priority = 1001, -- this plugin needs to run before anything else
+    opts = {
+      rocks = { "magick" },
+    },
+  },
+  {
+    "3rd/image.nvim",
+    config = function()
+      require("image").setup()
+    end,
+  }
 }
