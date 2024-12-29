ed7c516370f6486729451fa897da849046304886
Dale Lakes <dale.f.lakes@gmail.com>
2024-12-29 10:02:58 -0800

    Changes

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,4 +1,15 @@
 local wezterm = require("wezterm")
+local mux = wezterm.mux
+
+wezterm.on('gui-attached', function(domain)
+  -- maximize all displayed windows on startup
+  local workspace = mux.get_active_workspace()
+  for _, window in ipairs(mux.all_windows()) do
+    if window:get_workspace() == workspace then
+      window:gui_window():maximize()
+    end
+  end
+end)
 
 local config = {
 	default_prog = { "/usr/bin/zsh" },
