18805facb15ad97adc69747952ff051ef5349c7d
Dale Lakes <dale.f.lakes@gmail.com>
2025-01-03 09:52:01 -0800

    Fix stuff for MacOS. Simplify tmux

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,22 +1,22 @@
 local wezterm = require("wezterm")
 local mux = wezterm.mux
 
-wezterm.on('gui-attached', function(domain)
-  -- maximize all displayed windows on startup
-  local workspace = mux.get_active_workspace()
-  for _, window in ipairs(mux.all_windows()) do
-    if window:get_workspace() == workspace then
-      window:gui_window():maximize()
-    end
-  end
+wezterm.on("gui-attached", function(domain)
+	-- maximize all displayed windows on startup
+	local workspace = mux.get_active_workspace()
+	for _, window in ipairs(mux.all_windows()) do
+		if window:get_workspace() == workspace then
+			window:gui_window():maximize()
+		end
+	end
 end)
 
 local config = {
-	default_prog = { "/usr/bin/zsh" },
+	default_prog = { "/bin/zsh" },
 	font = wezterm.font("BerkeleyMono Nerd Font"),
-	color_scheme = "Kanagawa (Gogh)",
+	font_size = 14,
+	color_scheme = "Tokyo Night (Gogh)",
 	enable_tab_bar = false,
-	force_reverse_video_cursor = true,
 }
 
 return config
